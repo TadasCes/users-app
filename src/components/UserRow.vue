b53514265876bfa23f4b08b1e8ea78ef1ece5d79
VilkelisV <v.vilkelis97@gmail.com>
2020-12-28 13:32:03 +0200

    INIT project

---
+++ src/components/UserRow.vue
@@ -0,0 +1,14 @@
+<template>
+  <tr>
+    <td>Vardas</td>
+    <td>email</td>
+    <td>telefon</td>
+    <td>company</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'UsersRow',
+};
+</script>
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <table class="table table-striped table-bordered">
+    <thead class="thead-dark">
+      <tr>
+        <!-- TODO normalias ikonas sudet ir dydzius padaryt -->
+        <th scope="col">
+          Name
+        </th>
+        <th scope="col">
+          Email
+        </th>
+        <th scope="col">
+          Phone
+        </th>
+        <th scope="col">
+          Company name
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <UserRow
+        v-for="user in displayedList"
+        :key="user.name"
+        :user="user"
+        @delete-user="deleteUser"
+      ></UserRow>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'UsersTable',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
